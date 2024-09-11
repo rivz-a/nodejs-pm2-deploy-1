557e52f65d235419961a578e665a82c4faba1f06
Aleksandr Solonitskii <sabmailcom@gmail.com>
2024-09-11 23:12:13 +0500

    feat: add ecosystem + host

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
+    },
+  },
+};
